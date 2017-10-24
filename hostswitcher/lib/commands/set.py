675e913aec735b1f4b6f928ecc48d0fc319bc4aa
Giuseppe Iannelli <dev@giuseppeiannelli.it>
2017-10-25 01:35:49 +0200

    add cmd classes in lib.commands

---
+++ hostswitcher/lib/commands/set.py
@@ -0,0 +1,8 @@
+import hostswitcher.lib
+
+class set_command(object):
+
+    def __init__(self, args):
+        self.path_hosts = hostswitcher.lib.hosts_path()
+        self.file_name = 'hosts.default'
+        print('run %s command' % __name__)
